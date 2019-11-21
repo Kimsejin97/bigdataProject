aba22de14d3b04341197a3a0d371a1c97217e62d
김세진 <kimsejin@gimsejin-ui-MacBook-Pro.local>
2019-11-21 22:41:04 +0900

    배틀로그 중복sql

--- deduplication.sql
+++ deduplication.sql
@@ -1,16 +1,3 @@
 DROP TABLE IF EXISTS re_battlelog;
 create table re_battlelog
 select distinct * from battlelog;
-
-
-DROP TABLE IF EXISTS final_battlelog;
-
-
-create table final_battlelog
-select *
-from (select distinct battleTime, battle_teams_0_0_tag
-		from re_battlelog) as final;
-
-
-select * from re_battlelog;
-select * from final_battlelog;
