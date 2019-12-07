8a163480f68686a1b58d45145d19c6cd6af40d8b
Kimsejin97 <33508257+Kimsejin97@users.noreply.github.com>
2019-12-08 00:21:01 +0900

    Delete re_battelog.py

--- crawling_code/re_battelog.py
+++
@@ -1,9 +0,0 @@
-#battlelog테이블 중복제거, re_battlelog테이블 생성
-import DB_connection
-
-
-conn = DB_connection.engine.connect()
-drop_reBattlelog = conn.execute("DROP TABLE IF EXISTS re_battlelog")
-create_reBattlelog = conn.execute("create table re_battlelog select distinct * from battlelog")
-turncate_battlelog = conn.execute("Truncate battlelog")
-conn.close()
